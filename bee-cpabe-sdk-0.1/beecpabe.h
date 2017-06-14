452f2e7cf37dae67df7d153f579bb44b87ba7582
Wei-Tsung Su <ellington.su@gmail.com>
2017-06-14 11:06:43 +0800

    Revise beecpabe.h for API document

--- bee-cpabe-sdk-0.1/beecpabe.h
+++ bee-cpabe-sdk-0.1/beecpabe.h
@@ -1,37 +1,37 @@
 /*
     Include glib.h, pbc.h, pbc_random.h 
  */
-int setup(char* pubkey_path, char* mstkey_path);
+int setup(unsigned char* pk_path, unsigned char* mk_path);
 /*  Function Name: setup
  *  
  *  Purpose:
- *  Generate system parameters, a public key, and a master secret key
- *  for use with keygen, encrytion, decrption
+ *  Generate system parameters, a public key, and a master key
+ *  used in keygen, encrytion, decrption
  *
  *  Parameters:
- *  1. pubkey_path: the path of public key (Ex. ./public_key)
- *  2. mstkey_path: the path of master secret key (Ex. ./master_key)
+ *  1. pk_path: the path of public key (Ex. ./cpabe_publickey)
+ *  2. mk_path: the path of master key (Ex. ./cpabe_masterkey)
  *
  *  Return:
  *   0: success
- *  -1: failed
+ *  -1: fail
  */
 
-int keygen(char* pubkey_path, char* mstkey_path, char* prikey_path, int attr_no, ...);
+int keygen(unsigned char* pk_path, unsigned char* mk_path, unsigned char* pk_path, int attr_no, ...);
 /*  Function Name: keygen
  *  
  *  Purpose:
- *  Generate a personal key with the listed attributes using public key and master secret key
- *  If you want to know more please find the detail in this link 
+ *  Generate a secret key with the given attributes with public key and master key
+ *  About the key attributes, please refer the following link 
  *  http://acsc.cs.utexas.edu/cpabe/cpabe-keygen.html
  *
  *  Parameters:
- *  1. pubkey_path: the path of public key
- *  2. mstkey_path: the path of maskter key 
- *  3. prikey_path: the path of private personal key
+ *  1. pk_path: the path of public key
+ *  2. mk_path: the path of master key 
+ *  3. pk_path: the path of secret key
  *  4. attr_no: the number of attributes
- *  5. ...: a serial of attributes in two forms, non-numerical and numerical.
- *  (Ex. "boy", "student" are non-numerical form and "salary = 1000", "age >= 18" are numerical form)
+ *  5. ...: a serial of attributes in two forms, non-numerical and numerical, specified by string.
+ *  (Ex. "boy", "student" for non-numerical attributes and "salary = 1000", "age >= 18" for numerical attributes)
  *
  *  Return:
  *   0: success
@@ -39,18 +39,35 @@ int keygen(char* pubkey_path, char* mstkey_path, char* prikey_path, int attr_no,
  */
 
 int enc(unsigned char* pk_path, unsigned char* pt, unsigned char* policy_str, unsigned char** ct);
-int fenc(unsigned char* pk_path, unsigned char* pt_path, unsigned char* policy_str, unsigned char* ct_path);
 /*  Function Name: enc
  *  
  *  Purpose:
- *  Encrypt plaintext into ciphertext with public key and access policy using cpabe.  
+ *  Encrypt non-file plaintext into non-file ciphertext with public key and policy using CPABE.  
  *
  *  Parameters:
- *  1. pubkey_path: the path of public key
- *  2. plaintext_path: the path of plaintext
- *  3. access_policy: the access policy (Please refer http://acsc.cs.utexas.edu/cpabe/cpabe-enc.html)
+ *  1. pk_path: the path of public key
+ *  2. pt: the plaintext
+ *  3. policy_str: the access policy (Please refer http://acsc.cs.utexas.edu/cpabe/cpabe-enc.html)
  *  (Ex. "boy and salary > 1000")
- *  4. ciphertext_path: the path of ciphertext
+ *  4. ct: the ciphertext
+ *
+ *  Return:
+ *   n: the length of ciphertext 
+ *  -1: failed
+ */
+
+int fenc(unsigned char* pk_path, unsigned char* pt_path, unsigned char* policy_str, unsigned char* ct_path);
+/*  Function Name: fenc
+ *  
+ *  Purpose:
+ *  Encrypt file-based plaintext into file-based ciphertext with public key and policy using CPABE.  
+ *
+ *  Parameters:
+ *  1. pk_path: the path of public key
+ *  2. pt_path: the path of plaintext
+ *  3. policy_str: the access policy (Please refer http://acsc.cs.utexas.edu/cpabe/cpabe-enc.html)
+ *  (Ex. "boy and salary > 1000")
+ *  4. ct_path: the path of ciphertext
  *
  *  Return:
  *   0: success
@@ -58,16 +75,32 @@ int fenc(unsigned char* pk_path, unsigned char* pt_path, unsigned char* policy_s
  */
 
 int dec(unsigned char* pk_path, unsigned char* sk_path, unsigned char* ct, unsigned char** pt);
-int fdec(unsigned char* pb_path, unsigned char* sk_path, unsigned char* ct_path);
 /*  Function Name: dec
  *  
  *  Purpose:
- *  Decrypt ciphertext into plaintext with public key and private key using cpabe.  
+ *  Decrypt non-file ciphertext into non-file plaintext with public key and secret key using CPABE.  
  *
  *  Parameters:
- *  1. pubkey_path: the path of public key
- *  2. prikey_path: the path of private key
- *  3. ciphertext_path: the path of ciphertext
+ *  1. pk_path: the path of public key
+ *  2. sk_path: the path of secret key
+ *  3. ct: the ciphertext
+ *  4. pt: the plaintext
+ *
+ *  Return:
+ *   0: success
+ *  -1: failed
+ */
+
+int fdec(unsigned char* pb_path, unsigned char* sk_path, unsigned char* ct_path);
+/*  Function Name: fdec
+ *  
+ *  Purpose:
+ *  Decrypt file-based ciphertext into file-based plaintext with public key and secret key using CPABE.  
+ *
+ *  Parameters:
+ *  1. pk_path: the path of public key
+ *  2. sk_path: the path of secret key
+ *  3. ct_path: the path of ciphertext
  *
  *  Return:
  *   0: success
