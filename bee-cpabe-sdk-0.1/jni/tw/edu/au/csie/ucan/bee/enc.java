6335bdfed0f262e12ef9b8ee78ba6b7f4e4ece67
Wei-Tsung Su <ellington.su@gmail.com>
2018-05-14 21:08:23 +0800

    remove dup cpabe-sdk project

--- bee-cpabe-sdk-0.1/jni/tw/edu/au/csie/ucan/bee/enc.java
+++
@@ -1,9 +0,0 @@
-package tw.edu.au.csie.ucan.bee;
-
-class enc {
-  public static void main(String[] args) {
-    BeeJNI bee = new BeeJNI();
-    int len = bee.fenc("./mypk","./data.txt","jackie and s > 100","default");
-    System.out.print("ENC SUCCESS(JNI): " + len + "\n");
-  }	
-}
